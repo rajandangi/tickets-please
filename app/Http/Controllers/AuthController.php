8d66565e021e2337ffeb32fdc06bbdc8f531159f
rajandangi <rajandangi649@gmail.com>
2024-11-15 11:44:01 +1030

    EP02: Setup postman request

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ApiLoginRequest;
+use App\Traits\ApiResponses;
+
+class AuthController extends Controller
+{
+    use ApiResponses;
+
+    public function login(ApiLoginRequest $request){
+        return $this->ok($request->get('email'));
+    }
+
+    public function register(){
+        return $this->ok('Register');
+    }
+}
